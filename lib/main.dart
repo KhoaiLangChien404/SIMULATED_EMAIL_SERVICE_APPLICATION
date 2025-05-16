a949a71576f06bae68cdbf811d4d99ec786c3613
KhoaiLangChien404 <leductrung2021123@gmail.com>
2025-05-16 14:23:59 +0700

    connect firebase project to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // file sinh ra từ flutterfire configure
 import 'screens/inbox_screen.dart';
 import 'theme/app_theme.dart';
 
-void main() {
-  runApp(EmailApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Dành cho mọi nền tảng (Web cần FirebaseOptions)
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const EmailApp());
 }
 
 class EmailApp extends StatelessWidget {
@@ -16,7 +25,7 @@ class EmailApp extends StatelessWidget {
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
-      home: InboxScreen(),
+      home: const InboxScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
