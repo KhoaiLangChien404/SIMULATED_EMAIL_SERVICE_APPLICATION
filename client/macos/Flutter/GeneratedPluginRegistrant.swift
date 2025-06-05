bec7fe4ebec5677ef546dadce758e06e50f1f62f
KhoaiLangChien404 <leductrung2021123@gmail.com>
2025-06-05 20:04:38 +0700

    commit before push

--- client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,11 +9,9 @@ import file_picker
 import file_selector_macos
 import flutter_app_badger
 import flutter_local_notifications
-import irondash_engine_context
 import path_provider_foundation
 import quill_native_bridge_macos
 import shared_preferences_foundation
-import super_native_extensions
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -21,10 +19,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterAppBadgerPlugin.register(with: registry.registrar(forPlugin: "FlutterAppBadgerPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  IrondashEngineContextPlugin.register(with: registry.registrar(forPlugin: "IrondashEngineContextPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   QuillNativeBridgePlugin.register(with: registry.registrar(forPlugin: "QuillNativeBridgePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SuperNativeExtensionsPlugin.register(with: registry.registrar(forPlugin: "SuperNativeExtensionsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
